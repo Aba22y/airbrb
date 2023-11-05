1822d50c493ee6e97bb31cb65f71fa44e0159f4a
Ahmed Al Abassy <z5312664@ad.unsw.edu.au>
2023-11-05 23:13:41 +1100

    basic login and registration pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,33 @@
 import React from 'react';
+import { Login } from './components/Login'
+import { Register } from './components/Register'
+import { Dashboard } from './components/Dashboard'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Banner } from './components/Banner';
+
+function LandingPage () {
+  return (
+    <h1>
+      Hello World!
+    </h1>
+  )
+}
 
 function App () {
+  // ensure state awareness of token
+  const [token, setToken] = React.useState(null);
   return (
-    <>Let&apos;s go!</>
+    <>
+      <Router>
+      <Banner token={token} setToken={setToken}/>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<Login token={token} setToken={setToken}/>} />
+          <Route path="/register" element={<Register token={token} setToken={setToken} />} />
+          <Route path="/dashboard" element={<Dashboard token={token} setToken={setToken}/>} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
